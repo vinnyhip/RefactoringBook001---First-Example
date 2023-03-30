424387d221273a1a3af66383696cf8288bf09128
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 16:51:48 -0300

    Using *Replace Loop With Pipeline* inside totalAmount

--- main.js
+++ main.js
@@ -70,11 +70,8 @@ function statement(invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0);
     }
 }
 
