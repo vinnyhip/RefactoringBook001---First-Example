2af3fc65ff1347e402fd5dd8cc9bc37bceaefe2f
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 15:27:19 -0300

    Add a intermediate data structure inside statement function to pass data to renderPlainText function

--- main.js
+++ main.js
@@ -14,10 +14,11 @@ const _invoices = fs.readFileSync("data/invoices.json");
 const invoices = JSON.parse(_invoices);
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
+function renderPlainText(data, invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         // print line for this order
