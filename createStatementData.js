897e4d04244420f99e1b6a569e9f484e6c9b5813
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 18:30:08 -0300

    Using *Move Function* to move amount calculatio to PerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -3,6 +3,30 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40_000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30_000;
+                if (this.performance.audience > 20) {
+                    result += 10_000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+
+        return result;
+    }
 }
 
 module.exports = {
