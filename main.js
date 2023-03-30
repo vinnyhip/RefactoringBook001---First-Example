ea07dbc0408cf91ed73fa7ce9e66775302d52ad0
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 16:35:09 -0300

    Using *Move Function* to move volumeCreditsFor from renderPlainText function to statement function

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -53,6 +54,13 @@ function statement(invoice, plays) {
 
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 function renderPlainText(data) {
@@ -64,13 +72,6 @@ function renderPlainText(data) {
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
     function usd(aNumber) {
         return new Intl.NumberFormat(
             "en-US", 
@@ -81,7 +82,7 @@ function renderPlainText(data) {
     function totalVolumeCredits() {
         let volumeCredits = 0;
         for (let perf of data.performances) {
-            volumeCredits +=  volumeCreditsFor(perf);
+            volumeCredits +=  perf.volumeCredits;
         }
         return volumeCredits;
     }
