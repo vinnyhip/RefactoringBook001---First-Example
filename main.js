7fa9d5ab22242a7374eacf77f60d882d019a1089
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 16:33:11 -0300

    Using *Move Function* to move amountFor from renderPlainText function to statement function

--- main.js
+++ main.js
@@ -21,23 +21,14 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-}
-
-function renderPlainText(data) {
-    let result = `Statement for ${data.customer}\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
 
     function amountFor(aPerformance) {
         let result = 0;
@@ -62,6 +53,16 @@ function renderPlainText(data) {
 
         return result;
     }
+}
+
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
@@ -88,7 +89,7 @@ function renderPlainText(data) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
