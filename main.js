b2e7cfe4731c793c99e0fc108e1e9fcb82328451
Vinícius Hipólito <vinnyhip@gmail.com>
2023-03-30 17:26:00 -0300

    Write a html version of statement

--- main.js
+++ main.js
@@ -28,6 +28,24 @@ function renderPlainText(data) {
     return result;
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
+    for(let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += ` <td>${usd(perf.amount)}</td></tr> \n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
 function usd(aNumber) {
     return new Intl.NumberFormat(
         "en-US", 
@@ -36,3 +54,4 @@ function usd(aNumber) {
 }
 
 console.log(statement(invoices[0], plays))
+console.log(htmlStatement(invoices[0], plays))
